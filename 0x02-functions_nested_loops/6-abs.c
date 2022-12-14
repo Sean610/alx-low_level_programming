de737ef0275f31e3c1d8743688e7817ada369a5a
Sean Ohiomokhare <111086934+Sean610@users.noreply.github.com>
2022-12-14 11:34:52 +0000

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Computes absolute value of integers.
+ * @n: Integer operand/ argument.
+ *
+ * Return: absolute.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+	}
+	return (n);
+}
